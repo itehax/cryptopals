c4dcd170b16f7475343cdf9deb37d06c45043992
itehax <itehaxx@gmail.com>
2024-11-14 13:31:23 +0100

    update

--- set2/lv1.py
+++ set2/lv1.py
@@ -1,13 +1,17 @@
-def pkcs_7(s: str, size):
-    if len(s) < size:
-        pad = size - len(s)
-        return s + pad.to_bytes(1, "little") * pad
-    else:
-        return s[:size] + pkcs_7(s[size:], 16)
+def pkcs_7(s: bytes, size):
+    pad = size - (len(s) % size)
+    return s + bytes([pad]) * pad
 
-def un_pkcs_7(s:str,size):
+
+def un_pkcs_7(s: bytes):
     rep = s[len(s) - 1]
-    return s[:-rep]
-    
+    if all(rep == s[i] for i in range(len(s) - 2, len(s) - rep - 1, -1)):
+        return s[:-rep]
+    raise
+
+
 if __name__ == "__main__":
-    print(pkcs_7(b"YELLOW SUBMARINE", 16))
+    for i in range(33):
+        print(un_pkcs_7(pkcs_7(b"a" * i, 16)))
+
+    un_pkcs_7(b"a"*14 + b"\x01\x02")
